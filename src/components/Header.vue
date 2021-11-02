cec51c2df4430817b7cee9f8c278a1e6d01aae5d
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2021-11-01 23:40:50 -0300

    Criando navbar do header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <nav class="container">
+            <a href="/">
+                <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg
+" alt="WDev" id="logo">
+            </a>
+
+            <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg
+" alt="Abrir Menu" id="menu-button">
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -12,4 +20,25 @@ export default {
 
 <style scoped>
     
+    header{
+        background-color: var(--color-background-nav);
+        width: 100%;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    nav{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #logo{
+        width: 130px;
+    }
+
+    #menu-button{
+        width: 30px;
+    }
 </style>
